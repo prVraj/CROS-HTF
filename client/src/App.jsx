ece04f78a97a5428d3ce3e71b05f95be4bde8344
prVraj <vrajparikh169@gmail.com>
2024-02-10 07:15:50 +0530

    React Added in frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Vraj from "./components/Vraj";
+
+
+export default function App() {
+  return (
+    <div>
+      <Vraj/>
+    </div>
+  );
+}
---
+++ client/src/components/Vraj.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Vraj() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center text-blue-500">Vraj</h1>
+    </div>
+  )
+}
