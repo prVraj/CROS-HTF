0ec8a3515978c4cb415e6a630b0463edb0b8a403
Jainam Priteshkumar Shah <128902428+Jainam1182s@users.noreply.github.com>
2024-02-10 19:11:00 +0530

    'menu'

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 import {DB_NAME} from "../constants.js"
-
+     
 const ConnectDB = async() =>{
     try {
         const connectionInstance = await mongoose.connect(
